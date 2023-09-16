72ab07a0714096504de0d3b8d22cedbf0d92eded
JarbasAi <jarbasai@mailfence.com>
2023-09-16 23:18:43 +0100

    feat/vui

--- ovos_audio_transformer_plugin_ggwave/__init__.py
+++ ovos_audio_transformer_plugin_ggwave/__init__.py
@@ -7,6 +7,22 @@ from ovos_utils import create_daemon
 from ovos_utils.log import LOG
 from ovos_utils.messagebus import Message
 
+from ovos_workshop.skills.ovos import OVOSSkill
+from ovos_workshop.decorators import intent_handler
+
+
+class GGWaveSkill(OVOSSkill):
+
+    @intent_handler("ggwave.enable.intent")
+    def handle_enable_ggwave(self, message):
+        self.bus.emit(message.forward("ovos.ggwave.enable"))
+        self.speak_dialog("ggwave.enabled")
+
+    @intent_handler("ggwave.disable.intent")
+    def handle_disable_ggwave(self, message):
+        self.bus.emit(message.forward("ovos.ggwave.disable"))
+        self.speak_dialog("ggwave.disabled")
+
 
 # NOTE - could not get ggwave to work properly with the audio feed
 # ran out of time so just used a subprocess
@@ -33,11 +49,30 @@ class GGWavePlugin(AudioTransformer):
             "PIP:": self.handle_pip
         }
         self._ssid = None
+        self.vui = None
+        # TODO - autoenable if wifi has not been setup previously
+        self.user_enabled = False
 
     def bind(self, bus=None):
         """ attach messagebus """
         super().bind(bus)
+        # we load the voice interface as part of this plugin
+        # the skill interacts only via messagebus
+        self.vui = GGWaveSkill(bus=self.bus, skill_id="ggwave.openvoiceos")
+        self.bus.on("ovos.ggwave.enable", self.handle_enable)
+        self.bus.on("ovos.ggwave.disable", self.handle_disable)
+
         self.daemon = create_daemon(self.monitor_thread)
+
+    def handle_enable(self, message: Message):
+        self.user_enabled = True
+
+    def handle_disable(self, message: Message):
+        self.user_enabled = False
+
+    def shutdown(self):
+        if self.vui is not None:
+            self.vui.shutdown()
 
     def handle_skill(self, payload):
         if not payload.startswith("https://github.com/"):
